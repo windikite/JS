4c788e4207293b2f7739b9202a242f4d01c087a0
windikite <windikite@yahoo.com>
2024-09-17 12:45:46 -0400

    Create StringManipulation.js

---
+++ StringManipulation.js
@@ -0,0 +1,17 @@
+// Assignment 2
+// Task 1
+let message = "Hello, World!";
+console.log(`message length ${message.length}`)
+
+// Task 2
+console.log(`caps message ${message.toUpperCase()} lower message ${message.toLowerCase()}`)
+
+// Task 3
+let sentence1 = "The quick brown fox jumps over the lazy dog";
+let fox = sentence1.substring(16, 19);
+console.log(fox)
+
+// Task 4
+let sentence2 = "The quick brown fox";
+let word_array = sentence2.split(" ");
+console.log(word_array);
